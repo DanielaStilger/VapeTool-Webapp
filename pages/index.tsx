d0fdf82bf93b13829becc30b9d31cdc5b9607fec
Stanislaw Baranski <stan@stan.bar>
2022-10-10 23:56:43 +0200

    update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,75 +1,41 @@
 import * as React from 'react';
 import type { NextPage } from 'next';
-import { useState } from 'react';
-import { useRouter } from 'next/router';
-import { Grid, Button } from '@mui/material'
+import { Button, Box , Container, Typography } from '@mui/material'
 import { useAuth } from '../context/FirebaseAuthContext';
+import ProTip from '../src/components/ProTip';
+import Copyright from '../src/components/Copyright';
+import Link from '../src/Link';
 
 
 const Home: NextPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
-  const router = useRouter();
-  const { signInWithEmailAndPassword, firebaseUser: authUser, signOut } = useAuth();
+  const { firebaseUser: authUser, signOut } = useAuth();
   console.log("rendering Home")
 
-  const onSubmit = (event: any) => {
-    event.preventDefault();
-    console.log("onSubmit", email, password)
-    setError(null)
-    signInWithEmailAndPassword(email, password)
-      .then(authUser => {
-        console.log("Success. The user is created in firebase")
-        router.push('/logged_in');
-      })
-      .catch(error => {
-        setError(null)
-        setError(error.message)
-        console.log(error)
-      });
-  };
 
   return (
-    <Grid container>
-      <Grid item xs={12}>
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          {authUser ? <div>Hi {authUser.displayName}! You are logged in.</div> : <div>Not logged in</div>}
+        </Typography>
+        <Link href="/cloud" color="secondary">
+          Go to the Cloud page
+        </Link>
+
         <Button variant="contained" color="primary" onClick={signOut}>Logout</Button>
-      </Grid>
-      <Grid item xs={12}>
-          {authUser ? <div>Congratulations {authUser.displayName}! You are logged in.</div> : <div>Not logged in</div>}
-      </Grid>
 
-    </Grid>
-
-    // <Grid container spacing={2}>
-    //     <Grid item xs={12}>
-    //       <h2>Login</h2>
-    //     </Grid>
-    //     <Grid item xs={4}>
-    //     <TextField
-    //               type="email"
-    //               value={email}
-    //               onChange={(event) => setEmail(event.target.value)}
-    //               name="email"
-    //               id="loginEmail"
-    //               placeholder="Email" />
-    //     </Grid>
-    //     <Grid item xs={4}>
-    //     <TextField
-    //               type="password"
-    //               name="password"
-    //               value={password}
-    //               onChange={(event) => setPassword(event.target.value)}
-    //               id="loginPassword"
-    //               placeholder="Password" />
-    //     </Grid>
-    //     <Grid item xs={2}>
-    //     <Button onClick={onSubmit}>Login</Button>
-    //     </Grid>
-    //     <Grid item xs={2}>
-    //       No account? <Link href="/sign_up">Create one</Link>
-    //     </Grid>
-    //   </Grid>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
   );
 };
 
