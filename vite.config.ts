362fd2a13c8f10a9a54444cb5eb6e249a232f5f1
Stanislaw Baranski <stan@stan.bar>
2023-03-17 03:52:36 +0100

    remove cert

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import fs from 'fs';
 
 export default defineConfig({
     plugins: [react()],
-    server: {
-        https: {
-            key: fs.readFileSync('./.cert/key.pem'),
-            cert: fs.readFileSync('./.cert/cert.pem'),
-        },
-    },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, '/src'),
